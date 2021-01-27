729a28f5b04c862a2501167afffaeebc7d9b991a
Abdulrafay <abdulrafayn001@gmail.com>
2021-01-27 21:03:25 +0500

    Student Login and Registration Done

---
+++ student_login.php
@@ -0,0 +1,47 @@
+<?php
+    // ------------------------Functions--------------------------------------
+    session_start();
+    function phpAlert($msg) {
+        echo '<script type="text/javascript">alert("' . $msg . '")</script>';
+    }
+
+    // --------------------------------------------------------------------
+    if(isset($_POST['sub']))
+    {
+        $con = mysqli_connect('localhost','root','','Library Management System');
+        if($con)
+        {
+            $email=mysqli_real_escape_string($con,$_POST['email']);
+            $password= mysqli_real_escape_string($con,$_POST['password']);
+
+            $sql = "SELECT * FROM student_registration where email='$email' && password='$password'";
+            $result = mysqli_query($con,$sql);
+            $result=mysqli_fetch_assoc($result);
+
+            
+            if(empty($result))
+            {
+                header('location:student_login.php');
+            }
+            else
+            {
+                $sql = "SELECT username, email FROM student_registration where email='$email' && password='$password'";
+                $result = mysqli_query($con,$sql);
+                $result=mysqli_fetch_assoc($result);
+                
+                
+
+                $_SESSION['username']=$result['username'];
+                $_SESSION['email']=$result['email'];
+
+                $_SESSION['login']="yes";
+                
+                header('location:user_panel.php');
+            }
+        }
+        else{
+            phpAlert("Database Connection Error");
+        }
+        mysqli_close($con);
+    }
+?>
---
+++ student_login_form.php
@@ -0,0 +1,45 @@
+<?php
+    session_start();
+    if(isset($_SESSION['username']))
+    {
+        header("Location: user_panel.php" );
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/formstyle.css">
+    <title>Register</title>
+</head>
+<body>
+    <div class="main">
+        <div class="form-box">
+            <div class="button-box">
+                <div id="btn"></div>
+                <button type="button" id="login_btn" class="toggle-btn" onclick="login()">Login</button>
+                <button type="button" id="register_btn" class="toggle-btn" onclick="register()">Register</button>
+            </div>
+            
+            <form class="input-group" id="login" action="student_login.php" method="post">
+                <input type="email" name="email" class="input-field" placeholder="Email" required>
+                <input type="password" name="password" class="input-field" placeholder="Enter Password" required>
+                <input type="checkbox" class="check-box"><span>Remember Password</span>
+                <button type="submit" name="sub" class="submit-btn">Log in</button>
+            </form>
+            
+            <form class="input-group" id="register" action="student_registration.php" method="post">
+                <input type="text" name="user"class="input-field" placeholder="User Id" required>
+                <input type="email" name="email" class="input-field" placeholder="Email" required>
+                <input type="password" name="password" class="input-field" placeholder="Enter Password" required>
+                <input type="checkbox" name="agree" class="check-box"><span>I agree to to the terms and conditions.</span>
+                <button type="submit" name="sub" class="submit-btn">Register</button>
+            </form>
+
+        </div>
+        <script src="JS/register_login.js"></script>
+    </div>
+    
+</body>
+</html>
